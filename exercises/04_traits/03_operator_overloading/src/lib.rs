d24cbcc42d786fbf62d9df41453cc85f2586a8e2
Nate Brown <nravinbrown@gmail.com>
2025-01-18 17:12:33 -0600

    04/03

--- exercises/04_traits/03_operator_overloading/src/lib.rs
+++ exercises/04_traits/03_operator_overloading/src/lib.rs
@@ -1,5 +1,6 @@
 use std::cmp::PartialEq;
 
+#[derive(PartialEq)]
 struct Ticket {
     title: String,
     description: String,
@@ -7,8 +8,6 @@ struct Ticket {
 }
 
 // TODO: Implement the `PartialEq` trait for `Ticket`.
-
-impl PartialEq for Ticket {}
 
 #[cfg(test)]
 mod tests {
