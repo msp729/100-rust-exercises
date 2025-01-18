d218c914e15059ce4be5f1a17f1c61243855b1f4
Nate Brown <nravinbrown@gmail.com>
2025-01-18 17:28:49 -0600

    04/12

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +9,15 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+use core::ops::Add;
+
+impl Add for WrappingU32 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self::new(self.value + other.value)
     }
 }
 
