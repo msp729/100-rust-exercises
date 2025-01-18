5afcb3d4c724b6c502000717f51d1a9644167ad0
Nate Brown <nravinbrown@gmail.com>
2025-01-18 17:43:01 -0600

    04/14

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,46 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+use core::ops::Add;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct SaturatingU16(u16);
+
+impl From<u16> for SaturatingU16 {
+    fn from(x: u16) -> Self {
+        Self(x)
+    }
+}
+impl From<&u16> for SaturatingU16 {
+    fn from(x: &u16) -> Self {
+        Self(*x)
+    }
+}
+impl From<u8> for SaturatingU16 {
+    fn from(x: u8) -> Self {
+        Self(x as u16)
+    }
+}
+impl From<&u8> for SaturatingU16 {
+    fn from(x: &u8) -> Self {
+        Self(*x as u16)
+    }
+}
+impl From<&Self> for SaturatingU16 {
+    fn from(x: &Self) -> Self {
+        *x
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.0 == *other
+    }
+}
+
+impl<T: Into<Self>> Add<T> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, other: T) -> Self {
+        Self(self.0.saturating_add(other.into().0))
+    }
+}
