74db11875fd6aa1197fdc8555cb06324e9c3fafc
Nate Brown <nravinbrown@gmail.com>
2025-01-18 14:52:43 -0600

    01/00

--- exercises/01_intro/00_welcome/src/lib.rs
+++ exercises/01_intro/00_welcome/src/lib.rs
@@ -16,8 +16,8 @@
 // If you get stuck for more than 10 minutes on an exercise, grab a trainer! We're here to help!
 // You can also find solutions to all exercises in the `solutions` git branch.
 fn greeting() -> &'static str {
-    // TODO: fix me 👇
-    "I'm ready to __!"
+    // i think i know rust already, but it's good to ensure I can do exercises
+    "I'm ready to learn Rust!"
 }
 
 // Your solutions will be automatically verified by a set of tests.
