03a9a8dbdd84f79b9ce50ee00b8ddcbf344622ed
Nate Brown <nravinbrown@gmail.com>
2025-01-18 22:20:03 -0600

    06/02

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -11,11 +11,13 @@
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
 pub fn fibonacci(n: u32) -> u32 {
-    // TODO: implement the `fibonacci` function
-    //
-    // Hint: use a `Vec` to memoize the results you have already calculated
-    // so that you don't have to recalculate them several times.
-    todo!()
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 0..n {
+        b = a + b;
+        a = b - a;
+    }
+    a
 }
 
 #[cfg(test)]
