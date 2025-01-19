d03299dff1cd537bfe53eae107f2db682477995c
Nate Brown <nravinbrown@gmail.com>
2025-01-19 00:33:22 -0600

    07/04

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -1,9 +1,17 @@
 // TODO: Given a vector of integers, split it in two halves
 //  and compute the sum of each half in a separate thread.
 //  Don't perform any heap allocation. Don't leak any memory.
+use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let m = v.len() / 2;
+    let lv = &v[..m];
+    let rv = &v[m..];
+    thread::scope(|scope| {
+        let l = scope.spawn(|| lv.into_iter().sum::<i32>());
+        let r = scope.spawn(|| rv.into_iter().sum::<i32>());
+        l.join().unwrap() + r.join().unwrap()
+    })
 }
 
 #[cfg(test)]
