857953ad461e6670d2484dbc6b598707e97a8799
Nate Brown <nravinbrown@gmail.com>
2025-01-19 00:25:06 -0600

    07/02

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -4,7 +4,10 @@
 use std::thread;
 
 pub fn sum(slice: &'static [i32]) -> i32 {
-    todo!()
+    let mid = slice.len() / 2;
+    let left = thread::spawn(move || slice[..mid].into_iter().sum::<i32>());
+    let right = thread::spawn(move || slice[mid..].into_iter().sum::<i32>());
+    left.join().unwrap() + right.join().unwrap()
 }
 
 #[cfg(test)]
